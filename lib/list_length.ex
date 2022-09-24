d6a7ec6c5a795da2f31a3b25dd3e109025a8547a
EmilyGMG <emily.maria2010@hotmail.com>
2022-09-23 23:05:36 -0300

    done

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,12 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list), do: calc_length(list, 0)
+
+  defp calc_length([], acc) do
+    acc
+  end
+
+  defp calc_length([_head | tail], acc) do
+    acc = acc + 1
+    calc_length(tail, acc)
   end
 end
